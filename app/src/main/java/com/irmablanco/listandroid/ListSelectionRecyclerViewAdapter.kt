136cf4fa4cb10c46e03cfa900c12b80370707c5d
Irma <irblancofdz@gmail.com>
2018-06-04 01:25:59 +0200

    Fix issue, recycler view works properly now

--- app/src/main/java/com/irmablanco/listandroid/ListSelectionRecyclerViewAdapter.kt
+++ app/src/main/java/com/irmablanco/listandroid/ListSelectionRecyclerViewAdapter.kt
@@ -1,29 +1,37 @@
 package com.irmablanco.listandroid
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.ViewGroup
 
 /*
 * Our ListSelectionRecyclerViewAdapter extends a RecyclerView.Adapter
 * which receives a view holder
 * */
-class ListSelectionRecyclerViewAdapter: RecyclerView.Adapter<ListSelectionRecylerViewHolder>(){
+class ListSelectionRecyclerViewAdapter: RecyclerView.Adapter<ListSelectionRecyclerViewHolder>(){
 
-    /**
-     * We create an instance variable to contain our list of TODOs
-     * */
+
     val listTitles = arrayOf("Shopping List", "Chores", "Android Tutorials")
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionRecylerViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
     override fun getItemCount(): Int {
         return listTitles.size
     }
 
-    override fun onBindViewHolder(holder: ListSelectionRecylerViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onBindViewHolder(holder: ListSelectionRecyclerViewHolder, position: Int) {
+        //1. Check if we have a holder (cell)
+        if (holder != null){
+            holder.listPosition.text = (position + 1).toString()
+            holder.listTitle.text = listTitles[position]
+        }
     }
+    /**onCreateViewHolder provides a layout for us to use for each piece of data
+     * and that layout is create for each row
+     * */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionRecyclerViewHolder {
+        //We create an instance of our list_selection_view_holder layout
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_selection_view_holder, parent, false)
+        return ListSelectionRecyclerViewHolder(view)
+    }
+
+
 
 }
---
+++ app/src/main/java/com/irmablanco/listandroid/ListSelectionRecyclerViewHolder.kt
@@ -0,0 +1,11 @@
+package com.irmablanco.listandroid
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.TextView
+
+
+class ListSelectionRecyclerViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)  {
+    val listPosition = itemView.findViewById<TextView>(R.id.itemNumber) as TextView
+    val  listTitle = itemView.findViewById<TextView>(R.id.itemString) as TextView
+}
