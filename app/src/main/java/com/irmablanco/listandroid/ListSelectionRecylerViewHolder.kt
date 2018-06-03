929f84581a62add16c412f4a49a5c6703ef0ae04
Irma <irblancofdz@gmail.com>
2018-06-03 23:35:12 +0200

    Set up project

---
+++ app/src/main/java/com/irmablanco/listandroid/ListSelectionRecylerViewHolder.kt
@@ -0,0 +1,11 @@
+package com.irmablanco.listandroid
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+/*
+* Our class ListSelectionRecyclerViewHolder receives
+* a parameter of type View and extends a RecuclerView.ViewHolder which also receives a view
+* */
+class ListSelectionRecylerViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)  {
+}
